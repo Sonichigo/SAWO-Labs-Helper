fd314576dd8f2a10d4747aaa657b9471e1af83a4
Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>
2021-11-11 19:06:52 +0530

    Update scamLinkDetector.ts

--- src/commands/scamLinkDetector.ts
+++ src/commands/scamLinkDetector.ts
@@ -5,8 +5,6 @@ module.exports = {
         const member = msg.author.id;
         if (msg.content.toLowerCase().includes("nitro is back") || msg.content.toLowerCase().includes("i leave from cs:go") || msg.content.toLowerCase().includes("nitro")) {
             msg.delete();
-            msg.channel.send(`<@!${member}>`);
-            msg.channel.send(`https://c.tenor.com/IB97xsNe7-YAAAAC/dont-do-that-avengers.gif`);
         }
     }
 }
