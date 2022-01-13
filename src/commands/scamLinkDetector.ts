f413d35724de1e11b4154c5ed20b32903d649af1
Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>
2022-01-13 09:28:12 +0530

    Update scamLinkDetector.ts

--- src/commands/scamLinkDetector.ts
+++ src/commands/scamLinkDetector.ts
@@ -2,9 +2,10 @@ module.exports = {
     name: "scamLinkDetector",
     description: "Detects scam links",
     run: (msg: any) => {
-        const member = msg.author.id;
-        if (msg.content.toLowerCase().includes("nitro is back") || msg.content.toLowerCase().includes("i leave from cs:go") || msg.content.toLowerCase().includes("nitro")) {
-            msg.delete();
+        if (msg.content.toLowerCase().includes("discrode") || msg.content.toLowerCase().includes("i leave from cs:go") || msg.content.toLowerCase().includes("Who is first?") || msg.content.toLowerCase().includes("discrod") ||
+        msg.content.includes("nitro") || 
+        msg.content.toLowerCase().includes("n i t r o")) {
+          msg.delete();
         }
     }
 }
